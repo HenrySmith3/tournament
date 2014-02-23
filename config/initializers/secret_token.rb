3d1edba0be420e2e6761303010f105d884eb7be8
Henry Smith <henry.smith@gatech.edu>
2014-02-22 20:22:15 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tournament::Application.config.secret_token = '090524680cbbb2694125aca46c7c4a62077f403d1760f143057584acd0c6b5cb29ae963deb285653d681d70f751bf48b34063cb50d4845d3db3aa672964e9691'
